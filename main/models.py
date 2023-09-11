6aaf896c94de7403b1f5f1b4f67d5e0f3a098496
tiffanyadisuryo <tiffanyadisuryo@gmail.com>
2023-09-11 22:54:53 +0700

    modifikasi models.py, views.py, main.html

--- main/models.py
+++ main/models.py
@@ -3,5 +3,5 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=255)
     date_added = models.DateField(auto_now_add=True)
-    price = models.IntegerField()
+    amount = models.IntegerField()
     description = models.TextField()
--- main/views.py
+++ main/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
-        'name': 'Pak Bepe',
+        'name': 'Tiffany Lindy Adisuryo',
+        'npm' : '2206025136',
         'class': 'PBP A'
     }
 
