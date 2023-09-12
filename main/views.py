c7187443e5fc95fa4ce84a0646017709c5c1122e
tiffanyadisuryo <tiffanyadisuryo@gmail.com>
2023-09-12 20:50:21 +0700

    fixed inventory attempt 7

--- main/views.py
+++ main/views.py
@@ -6,7 +6,7 @@ def show_main(request):
         'name': 'Tiffany Lindy Adisuryo',
         'class': 'PBP D',
         'snacks' : [{'name' : "Chitato", 'amount' : "1", 'description' : "Chitato is a popular Indonesian brand of potato chips known for its wide range of bold and flavorful snack offerings."},
-                    {'name' : "Beng Beng", 'amount' : "20", 'description' : "CBeng Beng is a well-known Indonesian chocolate snack that combines layers of crispy wafer and creamy chocolate filling, offering a delightful and satisfying treat."},
+                    {'name' : "Beng Beng", 'amount' : "20", 'description' : "Beng Beng is a well-known Indonesian chocolate snack that combines layers of crispy wafer and creamy chocolate filling, offering a delightful and satisfying treat."},
                     {'name' : "Oreo", 'amount' : "5", 'description' : "Oreo is a globally recognized sandwich cookie brand known for its iconic combination of two chocolate-flavored wafers with a sweet cream filling."}]
     }
 
