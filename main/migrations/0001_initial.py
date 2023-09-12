a74055a493ca02e372bea71fa86f5879f8f7d189
tiffanyadisuryo <tiffanyadisuryo@gmail.com>
2023-09-12 08:57:35 +0700

    fixed bugs

--- main/migrations/0001_initial.py
+++ main/migrations/0001_initial.py
@@ -17,7 +17,7 @@ class Migration(migrations.Migration):
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=255)),
                 ('date_added', models.DateField(auto_now_add=True)),
-                ('price', models.IntegerField()),
+                ('amount', models.IntegerField()),
                 ('description', models.TextField()),
             ],
         ),
