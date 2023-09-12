bb3749c620a269d0230fdaf908fe960dea08042b
tiffanyadisuryo <tiffanyadisuryo@gmail.com>
2023-09-12 15:39:36 +0700

    added inventory to html

--- main/views.py
+++ main/views.py
@@ -4,7 +4,10 @@ from django.shortcuts import render
 def show_main(request):
     context = {
         'name': 'Tiffany Lindy Adisuryo',
-        'class': 'PBP D'
+        'class': 'PBP D',
+        'snacks' : [{'name' : "Chitato", 'amount' : "1", 'description' : "Chitato is a popular Indonesian brand of potato chips known for its wide range of bold and flavorful snack offerings."},
+                    {'name' : "Beng Beng", 'amount' : "20", 'description' : "CBeng Beng is a well-known Indonesian chocolate snack that combines layers of crispy wafer and creamy chocolate filling, offering a delightful and satisfying treat."},
+                    {'name' : "Oreo", 'amount' : "5", 'description' : "Oreo is a globally recognized sandwich cookie brand known for its iconic combination of two chocolate-flavored wafers with a sweet cream filling."}]
     }
 
     return render(request, "main.html", context)
